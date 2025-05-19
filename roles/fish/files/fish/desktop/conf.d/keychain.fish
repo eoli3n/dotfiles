7c72b0a156349fa0ac4d7ded5b657c1d1a00c921
eoli3n <jonathan.kirszling@runbox.com>
2025-05-19 08:41:34 +0200

    fix keychain spawn

--- roles/fish/files/fish/desktop/conf.d/keychain.fish
+++ roles/fish/files/fish/desktop/conf.d/keychain.fish
@@ -3,6 +3,6 @@ set -x GPG_TTY (tty)
 
 # https://github.com/fish-shell/fish-shell/issues/4583
 if status --is-interactive
-    keychain --eval --agents ssh --quiet -Q id_rsa | source
-    keychain --eval --agents gpg --quiet --gpg2 -Q | source
+    keychain --eval --quiet -Q id_rsa | source
+    keychain --eval --quiet --gpg2 -Q | source
 end
